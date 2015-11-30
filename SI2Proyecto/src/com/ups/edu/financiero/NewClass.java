d6d56f664b0d10312902a4fea3249acd756dc8b8
USUARIO <USUARIO@TOSHIBA>
2015-11-29 23:01:26 -0500

    commit de pruebas

--- SI2Proyecto/src/com/ups/edu/financiero/NewClass.java
+++ SI2Proyecto/src/com/ups/edu/financiero/NewClass.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package financiero;
+package com.ups.edu.financiero;
 
 /**
  *
  * @author USUARIO
  */
 public class NewClass {
+    ///dasdaszdsdas
     
 }
